7af8a396b861a3d0407a4871d6271c57d184f503
JuGalindo <99206803+JuGalindo@users.noreply.github.com>
2023-12-01 20:07:22 -0300

    Update palindromeDetection.sh

--- 02-usuario/palindromeDetection.sh
+++ 02-usuario/palindromeDetection.sh
@@ -4,13 +4,13 @@ regex="/.txt?$"
 
 if [[ $1 =~ $regex ]]; then 
 
-	for word in $( cat $1 ) do
+	for word in $( cat $1 ); do
 		if [ "$word" == "$(rev $word)" ]; then
 			echo "$word es palíndromo."
 		fi
 	done
 
-# echo -n "$str" | tac -s' '
+
 
 			
 
