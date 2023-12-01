99eaaf699175596a6d346eb217ed0eab2fd5e1bf
JuGalindo <99206803+JuGalindo@users.noreply.github.com>
2023-12-01 19:51:06 -0300

    Update analizartexto.sh

--- analizartexto.sh
+++ analizartexto.sh
@@ -5,7 +5,7 @@ echo "Hola, soy el analizador de texto. Que quiere hacer?"
 select opcion in "Palindromo" "Detector de mails" "Estadisticas" "Salir"
 do
  [ -z "$opcion" ] && echo "Elegir opcion valida!" && continue
- [ $REPLY == 1 ] && bash palindromo.sh $1 && continue
+ [ $REPLY == 1 ] && bash palindromeDetection.sh $1 && continue
  [ $REPLY == 2 ] && bash mailAddressDetection.sh $1 && continue
  [ $REPLY == 3 ] && bash statsWords.sh $1 && continue
  [ $REPLY == 4 ] && echo "Eligió salir!" && break
