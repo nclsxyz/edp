3b1a23708dfbad708d95ba0e1bf898484ae14bd8
JuGalindo <99206803+JuGalindo@users.noreply.github.com>
2023-12-02 12:44:38 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,17 +1,28 @@
 #!/bin/bash 
 
 PS3='Elija> '
-echo "Hola, soy el menu del TP. Que quiere hacer?"
+echo "Hola, soy el menu del TP. ¿Qué desea hacer?"
 select opcion in "Clasificar Archivos" "Monitorear Sistema" "Analizar Textos" "Salir"
 do
- [ -z "$opcion" ] && echo "Elegir opcion valida!" && continue
- [ $REPLY == 1 ] && bash clasificar.sh && continue
- [ $REPLY == 2 ] && bash monitor.sh && continue
- [ $REPLY == 3 ] && bash analizartexto.sh && read -p "Por favor, ingresa el nombre del archivo de texto: " archivo_de_texto
-            bash analizartexto.sh "$archivo_de_texto" && continue
- [ $REPLY == 4 ] && echo "Eligió salir!" && break
+    [ -z "$opcion" ] && echo "Elegir opción válida!" && continue
 
- echo "Opcion elegida: " $opcion
+    if [ $REPLY -eq 1 ]; then
+        bash clasificar.sh
+    elif [ $REPLY -eq 2 ]; then
+        bash monitor.sh
+    elif [ $REPLY -eq 3 ]; then
+        echo "Elegiste Analizar Textos."
+        read -p "Por favor, ingresa el nombre del archivo de texto: " archivo_de_texto
+        bash analizartexto.sh "$archivo_de_texto"
+    elif [ $REPLY -eq 4 ]; then
+        echo "Elegiste salir!"
+        break
+    else
+        echo "Opción inválida."
+    fi
+
+    echo "Opción elegida: $opcion"
 done
 
 exit 0
+
