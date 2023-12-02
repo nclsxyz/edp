28cf2ccefb2815974f993f05d6d33212fcca62b3
JuGalindo <99206803+JuGalindo@users.noreply.github.com>
2023-12-02 12:21:26 -0300

    Update palindromeDetection.sh

--- palindromeDetection.sh
+++ palindromeDetection.sh
@@ -4,7 +4,7 @@ echo "Analizando palíndromos en el archivo: $1"
 
 # Leer el archivo línea por línea y verificar si cada línea es un palíndromo
 while IFS= read -r line; do
-    reversed=$(echo "$line" | rev)
+    reversed=$(echo "$line" | awk '{ for (i=length; i!=0; i--) rev=rev substr($0,i,1);} END {print rev}')
     if [ "$line" == "$reversed" ]; then
         echo "$line es palíndromo."
     fi
