22d341881e6d7cbcf3f3081be9a958bd20554916
JuGalindo <99206803+JuGalindo@users.noreply.github.com>
2023-12-02 12:17:20 -0300

    Update palindromeDetection.sh

--- palindromeDetection.sh
+++ palindromeDetection.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Verificar si el argumento tiene la extensión .txt
-if [[ $1 =~ \.txt$ ]]; then 
-    echo "Analizando palíndromos en el archivo: $1"
+echo "Analizando palíndromos en el archivo: $1"
 
-    # Leer el archivo línea por línea
-    while IFS= read -r word; do
-        reversed=$(echo "$word" | rev)
-        if [ "$word" == "$reversed" ]; then
-            echo "$word es palíndromo."
-        fi
-    done < "$1"
-else
-    echo "El archivo no parece ser un archivo de texto con extensión .txt."
-fi
+# Leer el archivo línea por línea
+while IFS= read -r word; do
+    reversed=$(echo "$word" | rev)
+    if [ "$word" == "$reversed" ]; then
+        echo "$word es palíndromo."
+    fi
+done < "$1"
