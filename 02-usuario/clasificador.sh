61a747c243ab987fe360618bf64ee53e050b96b4
nicolas <nclsdlpl@gmail.com>
2023-12-16 19:52:04 -0300

    corregido clasificador de textos

--- 02-usuario/clasificador.sh
+++ 02-usuario/clasificador.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-
-inputs=input_downloader/
+echo "Bienvenido al clasificador de archivos. Aguarde mientras se procesan los archivos"
+inputs=input_downloader
 
 mkdir -p outputs_clasificador/
 mkdir -p outputs_clasificador/txt/ 
@@ -11,27 +11,26 @@ n_img=1
 n_txt=1
 n_snd=1
 
-for i in $(ls $inputs/)
+for i in $inputs/
 do
+       	dwnld=$(file $i) # archivos de la carpeta
+	if [[ $dwnld =~ "ASCII" ]]; then
+		echo "archivo de texto"
+   		mv "$i" outputs_clasificador/txt/ 
+   		n_txt=$((n_txt+1))
+   		mv outputs_clasificador/txt/$i outputs_clasificador/txt/${n_txt}.txt
+ 	elif [[ $dwnld =~ "RIFF" ]]; then
+		echo "archivo de audio"
+		mv "$i" outputs_clasificador/snd/
+        	n_snd=$((n_snd+1))
+		mv outputs_clasificador/snd/$i outputs_clasificador/snd/sonido${n_snd}.wav
+	 else
+		echo "archivo de imagen" 
+		mv "$i" outputs_clasificador/img/
+		n_img=$((n_img+1))
+		mv outputs_clasificador/img/$i outputs_clasificador/img/imagen${n_img}.png
 
- dwnld=$(file $1) # archivos de la carpeta
-
-
- if [[ $dwnld =~ "ASCII" ]]; then
-   mv $1 outputs_clasificador/txt/ 
-   mv outputs_clasificador/txt/$1 outputs_clasificador/txt/texto${n_txt}.txt
-   n_txt=$((n_txt+1))
-
- elif [[ $dwnld =~ "RIFF" ]]; then
-	mv $1 outputs_clasificador/snd/
-        mv outputs_clasificador/snd/$1 outputs_clasificador/snd/sonido${n_snd}.wav
-	n_snd=$((n_snd+1))
-
- else 
-	mv $1 outputs_clasificador/img/
-	mv outputs_clasificador/img/$1 outputs_clasificador/img/imagen${n_img}.png
-	n_img=$((n_img+1))
-
- fi 
- $cant_archivos=$(($cant_archivos-1))
+ 	fi
+#$cant_archivos=$(($cant_archivos-1))
+echo "Archivos movidos exitosamente."
 done
